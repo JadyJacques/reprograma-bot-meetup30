5a5dae0115061aabe397a3b73e4c7c9522bd6b49
JadyJacques <jadyjacques.nave@gmail.com>
2020-06-22 15:46:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { Random } = require('random-js');
+const random = new Random;
+const token = "1249813233:AAGJI-rkHgPSEE4EEUkmiUOWyZpO6_X6D3E";
+
+const  complimentSpeech = [
+  'Você manda muito bem',
+  'Arrasou',
+  'Você é 10!',
+  'Lacrou!!!!',
+  'TOP',
+]
+
+const bot = new TelegramBot(token, { polling: true })
+bot.on("new_chat_members", msg => {
+    bot.sendMessage(
+      msg.chat.id,
+      `Olá ${msg.new_chat_member.first_name}, bem vindo ao Meetup da Reprograma!`
+    );
+  });
+  
+  bot.on("message", msg => {
+    const chatId = msg.chat.id;
+  });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  let randomMessage = complimentSpeech[random.integer(0, complimentSpeech.length - 1)]
+    bot.sendMessage(chatId, randomMessage);
+});
